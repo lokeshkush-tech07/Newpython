1e3e442855c28cd43d0562ccd217cdeea945a327
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2025-11-21 19:11:56 +0530

    changes up to date

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,1 +1,24 @@
 # Q1. Write a program that asks the user for their name and age, then prints a sentence.
+'''
+name = str(input("Enter your Name: "))
+age = int(input("Enter your age: "))
+
+print("Hello",name,", You are", age, "years old.")
+'''
+
+# Q2. Take two numbers as input from the user and print their.Sum, difference, product, and quotient.
+'''
+a = float(input("Enter your first number:"))
+b = float(input("Enter your first number:"))
+
+sum = a+b
+difference = a-b
+product = a*b
+quotient = a/b
+
+print("The sum of ", a, "and", b, "is: ", sum)
+print("The difference of ", a, "and", b, "is: ", difference)
+print("The product of ", a, "and", b, "is: ", product)
+print("The quotient of ", a, "and", b, "is: ", quotient)
+
+'''
