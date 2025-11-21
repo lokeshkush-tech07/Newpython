b1246ed41dd68743830b25d3fc7b91ecc2ef15cc
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2025-11-21 18:12:28 +0530

    have some changes

--- new.py
+++ new.py
@@ -1,8 +1,11 @@
 #print("why you are not fixing my problem")
 #print("yess problem has fixed now")
 #Write a program that takes in 2 numbers (&) and print the average.
+'''
 a= int(input("Enter your first number: "))
 b= int(input("Enter your first number: "))
 
 avg=(a+b)/2
 print("Average of these two numbers is:", avg,  ".")
+'''
+
