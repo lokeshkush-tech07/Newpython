997f13b745909e12b8408e44ba3d48711154e550
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2025-11-22 09:12:44 +0530

    Updated Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -39,3 +39,61 @@ print("The average of number that you entered is: ", avg, ".")
 # print(type(c), c)
 # d = str(c)
 # print(type(d), d)
+
+#Evalute and print the result 
+'''
+x =10+3*2**2
+print(x) #these are operaters which will operate with their order arrange. Ans should be 22.
+'''
+'''
+#swaping the two number entered by user input.
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# before swaping
+print("a= ",a)
+print("b= ",b)
+
+#swaping
+a,b = b,a
+
+#After swaping print the numbers
+
+print("a= ",a)
+print("b= ",b)
+'''
+
+'''
+#Convert celcious temprature into farenhite
+
+a = str(input("Enter Celcious temprature: "))
+celsiustemp = float(a)
+FahrenheitTemp = (celsiustemp*(9/5))+32
+
+print("In Farenhite temp is: ", FahrenheitTemp)
+'''
+'''
+# Take radius from user and calculate the Area of the circle.
+radius = float(input("Enter the Radius of the Circle: "))
+PI = 3.14
+area = PI*(radius**2)
+print("The Area of the Circle is: ", area)
+'''
+'''
+#Calculate the simple interest
+p = float(input("Enter the principal ammount: "))
+r = float(input("Enter the Interest Rate: "))
+t = float(input("Enter Time duration: "))
+si = (p*r*t)/100
+
+print("Based on getting details , Simple interest is: ", si)
+'''
+
+'''
+# seprate the ntegerpart and fractional part by taking input nuber by an user
+num = float(input("Enter a decimal number with two places after decimal: "))
+integer = int(num)
+fractional = num - integer
+print("Integr part is: ", integer, ",and Fractional part is: ", fractional)
+'''
