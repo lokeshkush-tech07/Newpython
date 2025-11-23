28e7b8f327ecfcb0615b2aa3c3d4524e5a44b289
lokeshkush-tech07 <lokeshkushwaha7303@gmail.com>
2025-11-23 08:51:41 +0530

    updated Newpython

--- new.py
+++ new.py
@@ -8,5 +8,50 @@ b= int(input("Enter your first number: "))
 avg=(a+b)/2
 print("Average of these two numbers is:", avg,  ".")
 '''
-print("All are being fine")
+#print("All are being fine")
 
+# d = {
+#     "name":"Lokesh", 
+#     "studying":"B.sc", 
+#     "cgpa":"7.45"
+# }
+#print(d)
+
+# for key,value in d.items():
+#     print(key, value)
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# print(d.get("cgpa2"))
+# new_items = {"locatin":"Siwan"}
+# print(d.update(new_items))
+
+# print(d)
+
+# my_set = {1, 1, 4, 8, 2, 2, 6, 4, 4}
+
+# print(my_set)
+# print(type(my_set))
+# print(len(my_set))
+
+# s = {10, 20, 30}
+
+# s.add(40)
+# print(s)
+
+# s.remove(10)
+# print(s)
+
+# print(s.pop())
+
+# s.clear()
+# print(s)
+
+
+#union and intersection 
+
+A = {1, 3, 4, 2, 7, 5, 5}
+B = {2, 4, 7, 9, 2, 1, 4}
+
+print(A.union(B))
+print(A.intersection(B))
